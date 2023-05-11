8164bf504f7e69465f52d9239ccf7bd49a40c3b6
Ariflutfi <arif.kurniawan@students.amikom.ac.id>
2023-05-11 10:18:37 +0700

    menambahkan logika fragment

--- app/src/main/java/com/example/fragmentapp/MainActivity.java
+++ app/src/main/java/com/example/fragmentapp/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.fragmentapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnFragment1, btnFragment2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnFragment1 = findViewById(R.id.btnFragment1);
+        btnFragment2 = findViewById(R.id.btnFragment2);
+
+        btnFragment1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new FirstFragment());
+            }
+        });
+
+        btnFragment2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new SecondFragment());
+            }
+        });
+
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.FrameLayout, fragment);
+        transaction.commit();
     }
 }
